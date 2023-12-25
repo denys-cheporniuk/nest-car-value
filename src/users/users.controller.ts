e4496a78fbec8912796b216795647f639d67c9c7
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-12-25 14:46:24 +0200

    add serialization for user endpoint responses, avoid password in the response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,8 +12,11 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
