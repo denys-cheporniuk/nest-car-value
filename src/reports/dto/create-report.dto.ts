2adce4e2467dc93cfc7314dd8bda519b0fa63905
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-12-27 14:39:23 +0200

    implement reports module, conner reports with users

---
+++ src/reports/dto/create-report.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLatitude,
+  IsLongitude,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @Min(0)
+  @Max(1000000)
+  @IsNumber()
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './reports.entity';
+import { CreateReportDto } from './dto/create-report.dto';
+import { User } from '../users/users.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private repository: Repository<Report>,
+  ) {}
+
+  async create(values: CreateReportDto, user: User) {
+    const report = await this.repository.create(values);
+    report.user = user;
+
+    return this.repository.save(report);
+  }
+}
